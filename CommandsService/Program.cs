b6c8f8a4acb66de0bbe6d2dafd4aa8e443f45258
Sanman Chavan <58350884+sanmanchavan1991@users.noreply.github.com>
2022-05-14 00:45:47 +0530

    created platform service and started with command service

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,16 +1,12 @@
-using Microsoft.EntityFrameworkCore;
-using PlatformService.Data;
-
-
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(opt=>opt.UseInMemoryDatabase("InMem"));
-builder.Services.AddScoped<IPlatformRepo,PlatformRepo>();
+
 builder.Services.AddControllers();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,8 +20,6 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-PrepDb.PrepPopulation(app);
 app.MapControllers();
 
 app.Run();
-
