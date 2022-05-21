d4ea898e23981b5a49adae1e6ac3299a4b7a941a
Sanman Chavan <58350884+sanmanchavan1991@users.noreply.github.com>
2022-05-22 02:50:03 +0530

    connect with SQL

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -4,9 +4,23 @@ using Microsoft.Extensions.Configuration;
 using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
+IWebHostEnvironment environment = builder.Environment;
+
+if (environment.IsProduction())
+{
+    Console.WriteLine("--> Using SqlServer Db");
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+       opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+}
+else
+{
+    Console.WriteLine("--> Using InMem Db");
+
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+
+}
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(opt=>opt.UseInMemoryDatabase("InMem"));
-builder.Services.AddScoped<IPlatformRepo,PlatformRepo>();
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
@@ -14,7 +28,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
-            Console.WriteLine($"--> CommandService Endpoint {builder.Configuration["CommandService"]}");
+Console.WriteLine($"--> CommandService Endpoint {builder.Configuration["CommandService"]}");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -27,7 +41,7 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-PrepDb.PrepPopulation(app);
+PrepDb.PrepPopulation(app,environment.IsProduction());
 app.MapControllers();
 
 app.Run();
